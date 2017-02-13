Fixes small bug, rotation was done in wrong direction (clockwise, versus anti-clockwise)

skeleton_tools/skeleton_tools.py
@@ -590,7 +590,7 @@ class Skeleton(object):
         transformation matrix.
          Parameters
         ----------
-            transformation:    array, [3 x Z x X x Y]
+            transformation:    array, [3 x Z x Y x X]
                 Map with new coordination, e.g. coord [0, 0, 0] is mapped to transformation[:, 0, 0, 0]
 
         Notes
@@ -601,7 +601,7 @@ class Skeleton(object):
         for node_id, node_dict in self.nx_graph.nodes_iter(data=True):
             cur_pos_voxel = node_dict['position'].voxel.astype(np.int)
             x, y, z = cur_pos_voxel
-            z, x, y = transformation[:, z, x, y]
+            z, y, x = transformation[:, z, y, x]
             new_pos_voxel = np.array([x, y, z])
             new_pos_voxel = new_pos_voxel.astype(np.int)
 

skeleton_tools/skeleton_tools_test.py
@@ -348,20 +348,19 @@ class TestSkeletonTools(unittest.TestCase):
         # Create transformation matrix.
         transformation = augment.create_identity_transformation(nodes_bb)
 
-        # rotate around z axis 90 degree clockwise.
+        # Rotate around z axis 90 degree anti-clockwise.
         transformation += augment.create_rotation_transformation(
             nodes_bb,
             math.pi/2)
-
 
         test_skeleton.apply_transformation(transformation)
         pred_pos0 = test_skeleton.nx_graph.node[0]['position'].voxel
         pred_pos1 = test_skeleton.nx_graph.node[1]['position'].voxel
         pred_pos2 = test_skeleton.nx_graph.node[2]['position'].voxel
 
-        exp_pos0 = np.array([0, 2, 0])
-        exp_pos1 = np.array([0, 1, 1])
-        exp_pos2 = np.array([1, 0, 2])
+        exp_pos0 = np.array([2, 0, 0])
+        exp_pos1 = np.array([2, 1, 1])
+        exp_pos2 = np.array([1, 2, 2])
 
         self.assertTrue((pred_pos0 == exp_pos0).all())
         self.assertTrue((pred_pos1 == exp_pos1).all())